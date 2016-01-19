3859cf22f11d923b16f73e89f4fe832fc3caeede
Mikhail Matz <matz@utexas.edu>
2016-01-19 12:40:42 -0600

    comments added to script

--- nrifyCoauthors.R
+++ nrifyCoauthors.R
@@ -1,8 +1,14 @@
-home="Texas at Austin"
-coauthorFile="~/Documents/GRANTS/coauthors2012_2016.txt"
+
+# this script writes a tab-deimited table of unique coauthor names and affiliations
+# If many affiliations exist for a coauthor, the most recent one will be listed.
+# coauthors with no listed affiliations are dropped
+# by Mikhail Matz, matz@utexas.edu
+
+home="Texas at Austin" # phrase to recognize your home institution - I assuming we don't need to list those coauthors
+coauthorFile="~/Documents/GRANTS/coauthors2012_2016.txt" # output of Casey Dunn's script, also available at https://github.com/z0on/Misc/blob/master/fetchCoauthors.py
 
 co=read.table(coauthorFile,header=T,sep="\t")
-co=co[!is.na(co$Affiliation),]
+co=co[!is.na(co$Affiliation),] # remark this line if you want to list coauthors with no listed affiliation
 co=co[-(grep(home,co$Affiliation)),]
 str(co)
 names=unique(co$Name)
